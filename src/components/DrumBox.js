6dd4215c053632eaa82209e194e3bdaedb0c0298
kleyu <jackob.naskreski@gmail.com>
2018-07-27 21:25:25 +0200

    add all functionality, code not DRY

--- src/components/DrumBox.js
+++ src/components/DrumBox.js
@@ -8,31 +8,67 @@ export default class DrumBox extends Component {
 		return (
 			<div className="box" id="drum-machine">
 				<div id="display">
-					<DrumPad className="drum-pad" id="boom">
+					<DrumPad
+						className="drum-pad"
+						id="boom"
+						src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+					>
 						Q
 					</DrumPad>
-					<DrumPad className="drum-pad" id="clap">
+					<DrumPad
+						className="drum-pad"
+						id="clap"
+						src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+					>
 						W
 					</DrumPad>
-					<DrumPad className="drum-pad" id="hihat">
+					<DrumPad
+						className="drum-pad"
+						id="hihat"
+						src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+					>
 						E
 					</DrumPad>
-					<DrumPad className="drum-pad" id="kick">
+					<DrumPad
+						className="drum-pad"
+						id="kick"
+						src="https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"
+					>
 						A
 					</DrumPad>
-					<DrumPad className="drum-pad" id="openhat">
+					<DrumPad
+						className="drum-pad"
+						id="openhat"
+						src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+					>
 						S
 					</DrumPad>
-					<DrumPad className="drum-pad" id="rider">
+					<DrumPad
+						className="drum-pad"
+						id="rider"
+						src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+					>
 						D
 					</DrumPad>
-					<DrumPad className="drum-pad" id="snare">
+					<DrumPad
+						className="drum-pad"
+						id="snare"
+						src="http://wavlist.com/soundfx/027/drum_snare2.wav"
+					>
 						Z
 					</DrumPad>
-					<DrumPad className="drum-pad" id="tink">
+					<DrumPad
+						className="drum-pad"
+						id="tink"
+						src="https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"
+					>
 						X
 					</DrumPad>
-					<DrumPad className="drum-pad" id="tom">
+					<DrumPad
+						className="drum-pad"
+						id="tom"
+						src="https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"
+					>
 						C
 					</DrumPad>
 				</div>
--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -2,19 +2,68 @@ import React from "react";
 
 const DrumPad = props => {
 	const playSound = e => {
-		if (e.keyCode === 81) {
-			document.querySelectorAll(".clip")[0].play();
+		const soundLetter = document.querySelectorAll(".clip");
+		// switch case for different keycodes (not the best way to solve(maybe link id passed by parent?))
+		switch (e.keyCode) {
+			case 81:
+				soundLetter[0].play();
+				break;
+			case 87:
+				soundLetter[1].play();
+				break;
+			case 69:
+				soundLetter[2].play();
+				break;
+			case 65:
+				soundLetter[3].play();
+				break;
+			case 83:
+				soundLetter[4].play();
+				break;
+			case 68:
+				soundLetter[5].play();
+				break;
+			case 90:
+				soundLetter[6].play();
+				break;
+			case 88:
+				soundLetter[7].play();
+				break;
+			case 67:
+				soundLetter[8].play();
+				break;
+		}
+	};
+
+	const playClick = () => {
+		const letter = document.querySelectorAll("audio");
+
+		switch (props.id) {
+			case "boom":
+				letter[0].play();
+				break;
+			case "clap":
+				letter[1].play();
+				break;
+			case "hihat":
+				letter[2].play();
+				break;
+			case "kick":
+				letter[3].play();
+				console.log((document.getElementById("display").innerText = "cymbal"));
+				break;
 		}
 	};
 
 	return (
-		<div className="drum-pad" onKeyDown={playSound} tabIndex="0">
+		<div
+			className="drum-pad"
+			onKeyDown={playSound}
+			onClick={playClick}
+			tabIndex="0"
+		>
 			{props.children}
-			<audio
-				src="http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3"
-				className="clip"
-				id={props.children}
-			/>
+			<audio src={props.src} className="clip" id={props.children} />
 		</div>
 	);
 };
